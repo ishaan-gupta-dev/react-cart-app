a7f8100374d53fd1e4291d3390af0ab928788c97
Ishaan Gupta <121491626+ishaan-gupta-developer@users.noreply.github.com>
2023-12-03 17:43:08 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Cart from './Cart';
+import Navbar from './Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Cart />
+    </div>
+  );
+}
+
+export default App;
